80fde7940ceccca2704ee66d7c7ebc2e64cb0859
vahidinline <vahidinline@gmail.com>
2025-10-03 21:46:40 +0330

    update listner

--- index.js
+++ index.js
@@ -13,25 +13,22 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('hddi');
-});
-app.get('/health', (req, res) => {
-  res.status(200).send('OK');
-});
-// Routes
+app.get('/', (req, res) => res.send('hddi'));
+app.get('/health', (req, res) => res.status(200).send('OK'));
+
 app.use('/auth', authRoutes);
 app.use('/salons', salonRoutes);
 app.use('/salons/:salonId/employees', employeeRoutes);
 app.use('/salons/:salonId/services', serviceRoutes);
 app.use('/employees/:employeeId/availability', availabilityRoutes);
 
-// MongoDB Atlas connection
-const uri = process.env.MONGO_URI; // set in .env
+const uri = process.env.MONGO_URI;
+const PORT = process.env.PORT || 5001;
+
 mongoose
-  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(uri)
   .then(() => {
     console.log('✅ Connected to MongoDB Atlas');
-    app.listen(5001, () => console.log('🚀 Server running on port 5001'));
+    app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
   })
   .catch((err) => console.error('❌ MongoDB connection error:', err));
